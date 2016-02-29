0e98cc79fe0f75302ec5c7f3b883e45db3b55388
Azarias <azarias.boutin@gmail.com>
2016-02-29 16:02:09 +0000

    Update doc

--- actionlist.cpp
+++ actionlist.cpp
@@ -5,19 +5,21 @@ ActionList::ActionList(QObject *parent) :
 {
 }
 
-
 void ActionList::addAction(const QString &regex, const char *member)
 {
+	//Add a pair to the list
     actions_.append(QPair<QRegExp,const char*>(QRegExp(regex),member));
 }
 
 bool ActionList::triggerActions(QString &ref,QObject *objBase)
 {
+	//For eahc registered actions
     foreach (auto p, actions_) {
-        if(p.first.indexIn(ref) != -1){
+        if(p.first.indexIn(ref) != -1){//See if the regex matches
              return QMetaObject::invokeMethod(objBase,p.second,Qt::DirectConnection,
                                        Q_ARG(QRegExp,p.first),
                                        Q_ARG(QString,ref));
+             //If it does, call the function with the given context
         }
     }
 
--- actionlist.h
+++ actionlist.h
@@ -12,13 +12,31 @@ class ActionList : public QObject
     Q_OBJECT
 public:
     explicit ActionList(QObject *parent = 0);
+    /**
+     * @brief      add the action to the list of pair.
+     * The string is used to build the regex.
+     *
+     * @param  regex   the regex base
+     * @param  member  the name of the function to call if the regex matches
+     */
     void addAction(const QString &regex,const char *member);
+
+    /**
+     * @brief      trigger the first action when the
+     * string given in parameter matches
+     *
+     * @param      ref      the string to test
+     * @param      objBase  the context object to test
+     *
+     * @return     the return value of the function, or false if no function was called
+     */
     bool triggerActions(QString &ref, QObject *objBase);
 
 signals:
 public slots:
 
 private:
+	//All the action stored
     QList<QPair<QRegExp,const char*>> actions_;
 };
 
